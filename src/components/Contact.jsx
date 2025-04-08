ab0f8bf3ee674795270731f023317e47b5aaee52
mahamudul hasan seber <sebermh@gmail.com>
2025-04-09 02:45:22 +0600

    DaisyUi  SetUp Done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Contact = () => {
     return (
         <div>
             <h1 className='text-8xl text-amber-800'>Contacts Us</h1>
+            <Link to="joinUs"><li><button className="btn btn-outline btn-success">JoinUs</button></li></Link>
+            <Link to="login"><li><button className="btn btn-outline btn-success">Login</button></li></Link>
+            <Link to="register"><li><button className="btn btn-outline btn-success">Register</button></li></Link>
         </div>
     );
 };
